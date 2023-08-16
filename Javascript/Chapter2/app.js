8db97eafc119b3afc0bfb179059ce3e57f9f0650
SyedMunim <syedmuuni@gmail.com>
2023-08-16 19:22:36 +0500

    almost
    javascript continoue is 9 to 11 chapter

--- Javascript/Chapter2/app.js
+++ Javascript/Chapter2/app.js
@@ -1,157 +1,276 @@
 // // <----(Chapter 6 to 9)---->
+
+
 // // <----(Qno 01)---->
-// document.write('Result:')
-// var a='The value of a is:';
-// var x='the value of a is:';
+// 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
 
-// var b=10;
-// document.write ('</br>')
-// var c =a+' '+b;
-// document.write (c)
-// document.write ('</br>')
-// document.write ('.........................................');
-
-// document.write ('</br>');
-// document.write ('</br>');
-// var b= ++b;
-// var d = "The value of ++a is:";
-// document.write(d+' '+b);
-// document.write ('</br>');
-// document.write('Now'+' '+ x +' '+ b );
-// document.write ('</br>');
-// document.write ('</br>');
-
-// document.write ('</br>');
-// document.write ('</br>');
-// var d = "The value of a++ is:";
-// document.write(d+' '+b);
-// document.write ('</br>');
-// var y = 12;
-// var b = y++
-// document.write('Now'+' '+ x+' '+ b );
-// document.write ('</br>');
-// document.write ('</br>');
-
-// document.write ('</br>');
-// document.write ('</br>');
-// var b=--b;
-// var d = "The value of --a is:";
-// document.write(d+' '+b);
-// document.write ('</br>');
-// document.write('Now'+' '+ x +' '+ b );
-// document.write ('</br>');
-// document.write ('</br>');
-
-// document.write ('</br>');
-// document.write ('</br>');
-// var b=b--;
-// var d = "The value of a-- is:";
-// document.write(d+' '+b);
-// document.write ('</br>');
-// var b = 10
-// document.write('Now'+' '+ x +' '+ b );
+// // <----(Answer 01)---->
+// var a=prompt("Enter a Number?");
+// document.write("Result: <br>");
+// document.write("The Value of a is:"+ a);
+// document.write("<br> ........................................<br> ");
+// var b=++a;
+// document.write("<br>The Value of a++ is:"+ b);
+// document.write("<br>Now The Value of a++ is:"+ a);
+// a=++a;
+// document.write("<br><br><br>The Value of a++ is:"+ b);
+// document.write("<br>Now The Value of a is:"+a);
 
 
-// // <----(Chapter 6 to 9)---->
+// a=--a;
+// document.write("<br><br><br>The Value of a-- is:"+ a);
+// document.write("<br>Now The Value of a is:"+a);
+
+// document.write("<br><br><br>The Value of a-- is:"+ a);
+// a=--a;
+// document.write("<br>Now The Value of a is:"+a);
+
+
 // // <----(Qno 02)---->
-// var a = 2 , b= 1;
-// var result = --a - --b + ++b +b--;
-// // --a;
 
-// // --a - --b;
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
 
-// // --a - --b + ++b;
+// // <----(Answer 02)---->
 
-// // --a - --b + ++b + b--; 
+// document.write("a is "+a);
+// document.write("<br> b is "+b);
+// document.write("<br> result is "+result);
 
-// document.write('a is'+' '+a);
-// document.write ('</br>');
-// document.write('b is'+' '+b);
-// document.write ('</br>');
-// document.write('result is'+' '+result);
 
-// // <----(Chapter 6 to 9)---->
 // // <----(Qno 03)---->
-// var a = prompt('Enter your name');
-// alert('Welcome '+ a + ' to the Page');
+// 3. Write a program that takes input a name from user &
+// greet the user.
+// // <----(Answer 03)---->
+// var a =prompt("Welcome Your Name Here Please?");
+// document.write("Welcome " + a + " To The Page ")
 
-// // <----(Chapter 6 to 9)---->
 // // <----(Qno 05)---->
+// 5. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
 
-// function generateTable() {
-//     const numberInput = document.getElementById('numberInput');
-//     const tableContainer = document.getElementById('tableContainer');
-//     tableContainer.innerHTML = ''; // Clear previous content
 
-//     let number = numberInput.value ? parseInt(numberInput.value) : 5;
+// // <----(Answer 05)---->
 
-//     let tableHTML = '<table>';
-//     for (let i = 1; i <= 10; i++) {
-//         tableHTML += '<tr><td>' + number + ' x ' + i + '</td><td>=</td><td>' + (number * i) + '</td></tr>';
-//     }
-//     tableHTML += '</table>';
-
-//     tableContainer.innerHTML = tableHTML;
+// var num = prompt("Enter Number", "0") 
+// var num = parseInt(num) || 5;
+// document.write('<table border="1" cellspacing="0">');
+// for (i = 1; i <=10; i++) {
+//   document.write("<tr><td>" + num + " x " + i + " = " + num * i + "</td></tr>");
 // }
 
-// // Display default table on page load
-// generateTable();
 
-// // <----(Chapter 6 to 9)---->
+
+
+
+
 // // <----(Qno 06)---->
 
-// function calculateAndDisplay() {
-//     const subject1Marks = parseFloat(document.getElementById('subject1').value);
-//     const subject2Marks = parseFloat(document.getElementById('subject2').value);
-//     const subject3Marks = parseFloat(document.getElementById('subject3').value);
+// 6. Take
+// a) Take three subjects name from user and store them in 3
+// different variables.
+// b) Total marks for each subject is 100, store it in another
+// variable.
+// c) Take obtained marks for first subject from user and
+// stored it in different variable.
+// d) Take obtained marks for remaining 2 subjects from user
+// and store them in variables.
+// e) Now calculate total marks and percentage and show the
+// result in browser like this.(Hint: user table)
 
-//     const totalMarksPerSubject = 100;
 
-//     const totalMarks = subject1Marks + subject2Marks + subject3Marks;
-//     const percentage = (totalMarks / (totalMarksPerSubject * 3)) * 100;
 
-//     const resultContainer = document.getElementById('resultContainer');
-//     resultContainer.innerHTML = `
-//         <table border="1" align="center">
-//             <tr>
-//                 <th>Subject</th>
-//                 <th>Obtained Marks</th>
-//                 <th>Total Marks</th>
-//             </tr>
-//             <tr align="center">
-//                 <td>English</td>
-//                 <td>${subject1Marks}</td>
-//                 <td>${totalMarksPerSubject}</td>
-//             </tr>
-//             <tr align="center">
-//                 <td>Maths</td>
-//                 <td>${subject2Marks}</td>
-//                 <td>${totalMarksPerSubject}</td>
-//             </tr>
-//             <tr align="center">
-//                 <td>Urdu</td>
-//                 <td>${subject3Marks}</td>
-//                 <td>${totalMarksPerSubject}</td>
-//             </tr>
-//         </table>
-//         <p>Total Marks: ${totalMarks}</p>
-//         <p>Percentage: ${percentage.toFixed(2)}%</p>
-//     `;
-// }
+// // <----(Answer 06)-->--
 
-// // <----(Chapter 9 to 11)---->
-// // <----(Qno 01)---->
 
+
+//  var subject1 = prompt("Enter the name of the first subject:");
+//  var subject2 = prompt("Enter the name of the second subject:");
+//  var subject3 = prompt("Enter the name of the third subject:");
+
+
+//  var totalMarks = 100;
+//  var totalMarks1=100*3;
+
+
+//  var obtainedMarks1 = parseFloat(prompt("Enter obtained marks for " + subject1 + ":"));
+
+
+//  var obtainedMarks2 = parseFloat(prompt("Enter obtained marks for " + subject2 + ":"));
+//  var obtainedMarks3 = parseFloat(prompt("Enter obtained marks for " + subject3 + ":"));
+
+
+//  var totalObtainedMarks = obtainedMarks1 + obtainedMarks2 + obtainedMarks3;
+//  var percentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+//  var eachPercentage1=(obtainedMarks1/totalMarks)*100 +"%";
+//  var eachPercentage2=(obtainedMarks2/totalMarks)*100 +"%";
+//  var eachPercentage3=(obtainedMarks3/totalMarks)*100 +"%";
+
+
+//  document.write("<table border='0'>");
+//  document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentege</th></tr>");
+//  document.write("<tr><td>" + subject1 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks1 +"</td><td>"+ eachPercentage1  +"</td></tr>");
+//  document.write("<tr><td>" + subject2 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks2 + "</td><td>"+ eachPercentage2  +"</td></tr>");
+//  document.write("<tr><td>" + subject3 + "</td><td>" + totalMarks + "</td><td>" + obtainedMarks3 +"</td><td>"+ eachPercentage3  +"</td></tr>");
+//  document.write("<tr><th>" +"        "+ "</th><th>" + totalMarks1 + "</th><th>" + totalObtainedMarks +"</th><th>"+ percentage.toFixed(2) + "%</th></tr>");
+
+//  document.write("</table>");
+
+
+
+
+
+// <----(Chapter 9 to 11)---->
+// <----(Qno 01)---->
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// // <----(Answer 01)-->--
 // var city = prompt('Enter your city')
 // if(city === 'Karachi'){
 //     alert('“Welcome to city of lights”')
+
 // }
 
-// // <----(Qno 02)---->
+// <----(Qno 02)---->
 
-var gender = prompt('Enter your gender')
-if(gender === 'male'){
-    alert('Good Morning Sir')
-}else("Good Morning ma'am")
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
 
-// // <----(Qno 03)---->
+// // <----(Answer 02)-->--
+// var gender = prompt('Enter your gender')
+// if(gender === 'male'){
+//     alert('Good Morning Sir')
+// }else("Good Morning ma'am")
+
+// <----(Qno 03)---->
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+// // <----(Answer 03)-->--
+
+
+// var color = prompt("Enter the color of the road traffic signal (red, yellow, or green):");
+
+// Converting input to lowercase for case-insensitive comparison
+// color = color.toLowerCase();
+
+
+
+// if (color === "red") {
+//     document.write("<p>STOP! The road signal is RED.</p>");
+// } else if (color === "yellow") {
+//     document.write("<p>CAUTION! The road signal is YELLOW.</p>");
+// } else if (color === "green") {
+//     document.write("<p>GO! The road signal is GREEN.</p>");
+// } else {
+//     document.write("<p>Invalid input. Please enter a valid color.</p>");
+// }
+
+
+
+// / <----(Qno 04)---->
+
+// Write a program to take input remaining fuel in car (in 
+//     litres) from user. If the current fuel is less than 0.25litres, 
+//     show the message “Please refill the fuel in your car”
+// // <----(Answer 04)-->--
+
+// var remainingFuel = parseFloat(prompt("Enter the remaining fuel in your car (in litres):"));
+
+// // Checking if the remaining fuel is less than 0.25 litres
+// if (remainingFuel < 0.25) {
+//     document.write("<p>Please refill the fuel in your car.</p>");
+// } else {
+//     document.write("<p>You have enough fuel in your car.</p>");
+// }
+
+// / <----(Qno 05)---->
+
+// // // <----(Answer 05)-->--
+// Run this script, & check whether alert message would be
+// displayed or not. Record the outputs.
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+
+// / <----(Qno 07)---->
+// Guess game:
+// Store a secret number (ranging from 1 to 10) in a variable.
+// Prompt user to guess the secret number.
+// a. If user guesses the same number, show “Bingo! Correct
+// answer”.
+// b. If the guessed number +1 is the secret number, show
+// “Close enough to the correct answer”.
+
+// // // <----(Answer 07)-->--
+// Store a secret number (ranging from 1 to 10)
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// Prompt user to guess the secret number
+// var userGuess = parseInt(prompt("Guess the secret number (between 1 and 10):"));
+
+// Check if user's guess is correct or close
+// if (userGuess === secretNumber) {
+//     document.write("<p>Bingo! Correct answer.</p>");
+// } else if (userGuess === secretNumber + 1 || userGuess === secretNumber - 1) {
+//     document.write("<p>Close enough to the correct answer.</p>");
+// } else {
+//     document.write("<p>Sorry, your guess is not correct.</p>");
+// }
+
+
+// / <----(Qno 08)---->
+
+// Write a program to check whether the given number is
+// divisible by 3. Show the message to the user if the number 
+// is divisible by 3.
+// // // <----(Answer 08)-->--
